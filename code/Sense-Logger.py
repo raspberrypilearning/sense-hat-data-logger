82aa0fb5ae6b7aa7d7b748ed58743355f34681b2
James Robinson <James Robinson>
2015-09-01 17:25:57 +0100

    Updated data log code

--- code/Sense-Logger.py
+++ code/Sense-Logger.py
@@ -1,15 +1,40 @@
-import time
+from datetime import datetime
 from sense_hat import SenseHat
 
 sense = SenseHat()
 
-while True:
-    temp = sense.get_temperature()
-    hum = sense.get_humidity()
-    pressure = sense.get_pressure()
-    yaw,pitch,roll = sense.get_orientation().values()
-    x,y,z = sense.get_accelerometer_raw()
-    print (temp,hum,pressure,yaw,pitch,roll)
+def get_sense_data():
+    #Get the temperature and round the figure to 2 decimal places
+    temperature = round(sense.get_temperature(),2)
 
-    time.sleep(0.2)
+    #Get the humidity and round the figure to 2 decimal places
+    humidity = round(sense.get_humidity(),2)
+
+    #Get the pressure and round the figure to 2 decimal places
+    pressure = round(sense.get_pressure())
+
+    #Get the yaw,pitch,roll data from the orientation sensor
+    yaw,pitch,roll = sense.get_orientation().values()
+
+    #Round the yaw,pitch,roll data to 2 decimal places
+    yaw = round(yaw,2)
+    pitch = round(pitch,2)
+    roll = round(pitch,2)
+
+    #Get the acceleration in the 3 axes and then round them to 2 decimal places
+    x,y,z = sense.get_accelerometer_raw().values()
+    x = round(x,2)
+    y = round(y,2)
+    z = round(z,2)
+
+    #Get the compass data and round to 2 decimal places
+    compass = round(sense.get_compass(),2)
+
+    return datetime.now(),temperature,humidy,pressure,yaw,pitch,roll,x,y,z,compass
+
+while True:
+    sensor_values = ",".join(str(bit) for bit in get_sense_data())
+    print(sensor_values)
+    with open("sense-data.csv","a") as f:
+        f.write(sensor_values + "\n")
 
