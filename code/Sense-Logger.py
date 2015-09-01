99448691a1bb21fc8a628a1743e8e60c406a95f7
James Robinson <james.robinson@raspberrypi.org>
2015-09-01 15:01:59 +0000

    More sensors

--- code/Sense-Logger.py
+++ code/Sense-Logger.py
@@ -3,9 +3,13 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-#temp = sense.get_temperature()
-#hum = sense.get_humidity()
-#pressure = sense.get_pressure()
-yaw = sense.get_orientation()["pitch"]
-print (yaw)
+while True:
+    temp = sense.get_temperature()
+    hum = sense.get_humidity()
+    pressure = sense.get_pressure()
+    yaw,pitch,roll = sense.get_orientation().values()
+    x,y,z = sense.get_accelerometer_raw()
+    print (temp,hum,pressure,yaw,pitch,roll)
+
+    time.sleep(0.2)
 
