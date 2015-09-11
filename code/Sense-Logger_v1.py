90e4591827c5e007266372c9d247449fa660448e
James Robinson <james.robinson@raspberrypi.org>
2015-09-11 14:15:16 +0100

    Update Sense-Logger_v1.py

--- code/Sense-Logger_v1.py
+++ code/Sense-Logger_v1.py
@@ -1,41 +1,39 @@
-###### Import Libraries ######
+##### Libraries #####
 from sense_hat import SenseHat
-from datetime import datetime
 
-###### Settings ######
+##### Logging Settings #####
 
 
-###### Functions ######
-def get_sense_data():   
-  sense_data=[]
-  sense_data.extend([sense.get_temperature_from_humidity(),sense.get_temperature_from_pressure()])
-  sense_data.append(sense.get_humidity())
-  sense_data.append(sense.get_pressure())
-  
-  yaw,pitch,roll = sense.get_orientation().values()        
-  sense_data.extend([pitch,roll,yaw])
-  
-  mag_x,mag_y,mag_z = sense.get_compass_raw().values()
-  sense_data.extend([mag_x,mag_y,mag_z])
-  
-  x,y,z = sense.get_accelerometer_raw().values()
-  sense_data.extend([x,y,z])
-  
-  gyro_x,gyro_y,gyro_z = sense.get_gyroscope_raw().values()
-  sense_data.extend([gyro_x,gyro_y,gyro_z])
-      
-  sense_data.append(datetime.now())
-  return sense_data
+## Function to collect data from the sense hat and build a string
+def get_sense_data():
+    sense_data=[]
+    
+    sense_data.append(sense.get_temperature_from_humidity())
+    sense_data.append(sense.get_temperature_from_pressure())
+    sense_data.append(sense.get_humidity())
+    sense_data.append(sense.get_pressure())
+    
+    yaw,pitch,roll = sense.get_orientation().values()        
+    sense_data.extend([pitch,roll,yaw])
+
+    mag_x,mag_y,mag_z = sense.get_compass_raw().values()
+    sense_data.extend([mag_x,mag_y,mag_z])
+
+    x,y,z = sense.get_accelerometer_raw().values()
+    sense_data.extend([x,y,z])
+
+    gyro_x,gyro_y,gyro_z = sense.get_gyroscope_raw().values()
+    sense_data.extend([gyro_x,gyro_y,gyro_z])
+    
+    sense_data.append(datetime.now())
      
+    return sense_data
 
-###### Main Program ######
+## Main Program
 sense = SenseHat()
-filename = "SenseLog-" + str(datetime.now()) + ".csv"
 
 while True:
-  sense_data = get_sense_data()
-  output_string = ",".join(str(value) for value in sense_data)
-  print(output_string)
-
-  with open(filename,"a") as f:
-    f.write(output_string + "\n")
+    sense_data = get_sense_data()
+    print(sense_data)
+    
+    
